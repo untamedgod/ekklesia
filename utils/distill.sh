7d78b6d8a2605ffa5c84fd8a27dafc5879148242
devlush <devlush@lemmaplex.org>
2024-12-24 00:05:24 -0500

    enhance distill script

--- utils/distill.sh
+++ utils/distill.sh
@@ -27,7 +27,7 @@ for i in $(echo "${accum}" | tr ' ' '\n'); do
 
   # index pages from pdf
   while read line; do
-    echo "${line}"
+    #echo "${line}"
     read -r page_begin <<< $(echo "${line}" | grep -oE '#page=[0-9]+' | grep -oE '[0-9]+')
     index_set="${index_set}${index_set:+ }${page_begin}"
   done <<-__EOT__
@@ -39,8 +39,29 @@ for i in $(echo "${accum}" | tr ' ' '\n'); do
 	  | sort -t '=' -n -k2 \
 	)
 	__EOT__
+
+  compl_set=''
+
+  # complement set of page numbers
+  echo -n "extract_image() ${i}.pdf pg 1"
+  for j in $(seq 2 ${page_begin}); do
+    if [[ -z $(echo -n "${j} ${index_set}" | tr ' ' '\n' | sort -n | uniq -d) ]]; then
+      echo -n ",${j}"
+      compl_set="${compl_set}${compl_set:+ }${j}"
+    else
+      echo
+      echo -n "extract_image() ${i}.pdf pg ${j}"
+    fi
+  done
+  echo
+  echo
   echo
   echo "${index_set}"
   echo
+  echo "${compl_set}"
+  echo
+
 done
+
+
 
