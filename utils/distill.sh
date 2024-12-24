27435001d2c7729183779139f60ec4f55df449bb
devlush <devlush@lemmaplex.org>
2024-12-24 22:13:32 +0000

    wip, distill.sh

--- utils/distill.sh
+++ utils/distill.sh
@@ -32,68 +32,71 @@ _catalog_namespaces() {
   NAMESPACE_CATALOG="${accum}"
 }
 
+_enumerate_indices() {
+
+  for namespace in $(echo "$NAMESPACE_CATALOG" | tr ' ' '\n'); do
+    echo "${namespace}"
+
+    index_set=''
+
+    # index pages from pdf
+    while read line; do
+      #echo "${line}"
+      read -r sermon_page_begin  <<< $(echo "${line}" | grep -oE '#page=[0-9]+' | grep -oE '[0-9]+')
+      read -r sermon_title       <<< $(echo "${line}" | grep -oE '.*>.*</a>' )
+      index_set="${index_set}${index_set:+ }${sermon_page_begin}"
+    done <<-__EOT__
+	  $( \
+	    cat ./Dads_Sermons_Alphabetical_Rev3.html \
+	    | grep 'href="Dads_Sermons' | sed 's@.*<A href="@@' \
+	    | sort \
+	    | grep "${namespace}" \
+	    | sort -t '=' -n -k2 \
+	  )
+		__EOT__
+
+    compl_set=''
+
+    # complement set of page numbers
+    for j in $(seq 1 ${sermon_page_begin}); do
+      if [[ -z $(echo -n "${j} ${index_set}" | tr ' ' '\n' | sort -n | uniq -d) ]]; then
+        compl_set="${compl_set}${compl_set:+ }${j}"
+      fi
+    done
+
+    echo
+    echo
+    echo
+    echo "${index_set}"
+    echo
+    echo "${compl_set}"
+    echo
+  done
+}
+
+
+
+#  compl_set=''
+
+  # complement set of page numbers
+#  echo -n "extract_image() ${i}.pdf pg 1"
+#  for j in $(seq 2 ${sermon_page_begin}); do
+#    if [[ -z $(echo -n "${j} ${index_set}" | tr ' ' '\n' | sort -n | uniq -d) ]]; then
+#      echo -n ",${j}"
+#      compl_set="${compl_set}${compl_set:+ }${j}"
+#    else
+#      echo
+#      #echo -n "extract_image() ${i}.pdf pg ${j}"
+#      _extract_sermon "${i}.pdf" "${j}" "xxyyzz"
+#    fi
+#  done
+
+
+
+
+
+
 _catalog_namespaces
-
-# <A href="Dads_Sermons_Warren2_i.pdf#page=10" target="contents" >Acts 10:38</a><br>
-
-for namespace in $(echo "$NAMESPACE_CATALOG" | tr ' ' '\n'); do
-  echo "${namespace}"
-
-  index_set=''
-
-  # index pages from pdf
-  while read line; do
-    #echo "${line}"
-    read -r sermon_page_begin  <<< $(echo "${line}" | grep -oE '#page=[0-9]+' | grep -oE '[0-9]+')
-    read -r sermon_title       <<< $(echo "${line}" | grep -oE '.*>.*</a>' )
-    index_set="${index_set}${index_set:+ }${sermon_page_begin}"
-  done <<-__EOT__
-	$( \
-	  cat ./Dads_Sermons_Alphabetical_Rev3.html \
-	  | grep 'href="Dads_Sermons' | sed 's@.*<A href="@@' \
-	  | sort \
-	  | grep "${namespace}" \
-	  | sort -t '=' -n -k2 \
-	)
-	__EOT__
-
-  compl_set=''
-
-  # complement set of page numbers
-  for j in $(seq 1 ${sermon_page_begin}); do
-    if [[ -z $(echo -n "${j} ${index_set}" | tr ' ' '\n' | sort -n | uniq -d) ]]; then
-      compl_set="${compl_set}${compl_set:+ }${j}"
-    fi
-  done
-
-  echo
-  echo
-  echo
-  echo "${index_set}"
-  echo
-  echo "${compl_set}"
-  echo
-
-
-  break
-
-  compl_set=''
-
-  # complement set of page numbers
-  echo -n "extract_image() ${i}.pdf pg 1"
-  for j in $(seq 2 ${sermon_page_begin}); do
-    if [[ -z $(echo -n "${j} ${index_set}" | tr ' ' '\n' | sort -n | uniq -d) ]]; then
-      echo -n ",${j}"
-      compl_set="${compl_set}${compl_set:+ }${j}"
-    else
-      echo
-      #echo -n "extract_image() ${i}.pdf pg ${j}"
-      _extract_sermon "${i}.pdf" "${j}" "xxyyzz"
-    fi
-  done
-
-
-done
-
+_enumerate_indices
 
 
